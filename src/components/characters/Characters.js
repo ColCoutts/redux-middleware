60b562e2409c13e013dc274d8a6b12e6a140d1c5
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-30 09:41:52 -0700

    characters component created

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,4 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Character from './Character';
 
-function c
+function Characters({ characters }) {
+  const characterList = characters.map(character => (
+    <li key={character.id}>
+      <Character character={character} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {characterList}
+    </ul>
+  );
+}
+
+Characters.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default Characters;
