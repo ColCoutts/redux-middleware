c30aa7c9cff97ca99c481e13d32dbf624dc6613b
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-30 14:18:59 -0700

    character detail page created

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import AllCharacters from '../containers/AllCharacters';
 
 export default function App() {
   return (
-    <AllCharacters />
+    <Router>
+      <Switch>
+        <Route path="/" component={AllCharacters}/>
+      </Switch>
+    </Router>
   );
 }
---
+++ src/components/characters/CharacterDetail.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CharacterDetail({ character }) {
+  const {
+    name,
+    image,
+    affiliation
+  } = character;
+
+  return (
+    <section>
+      <h2>{name}</h2>
+      <img src={image} />
+      <p>{affiliation}</p>
+    </section>
+  );
+}
+
+CharacterDetail.propTypes = {
+  character: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    affiliation: PropTypes.string.isRequired
+  }).isRequired
+};
